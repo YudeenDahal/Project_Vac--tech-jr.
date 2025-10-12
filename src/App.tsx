592742f44e27d2ec80236667fa05b42202f31cc6
Manisha Magar <magarmanisha248@gmail.com>
2025-10-12 13:23:05 +0545

    feat: updating app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
+
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import AboutUs from "./pages/AboutUs";
+import HomePage from "./pages/HomePage";
+
+const AppContent = () => {
+  return (
+    <div
+      className={` flex flex-col min-h-screen transition-colors duration-300 `}
+    >
+      <div className="sticky top-0 z-50">
+        <NavBar />
+      </div>
+
+      <main>
+        <Routes>
+           <Route path="/" element={<HomePage />} />
+          <Route path="/about-us" element={<AboutUs />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <>
-      <AboutUs />
-    </>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/*" element={<AppContent />} />
+    </Routes>    
+    </BrowserRouter>
+    
+
+
   );
 }
 
